9e16316b68fc3ebd8293dabb347797040926ca58
Michael Park <mparkcode@gmail.com>
2020-09-16 11:57:08 +0000

    added error handling for it env.py does not exist

--- django_example/settings.py
+++ django_example/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-import env
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
